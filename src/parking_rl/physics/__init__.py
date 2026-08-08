09ab128e202c9b8ef833eeae9418ded59bdf9ac9
Lemonterminator <jianglinan1030@gmail.com>
2026-08-08 16:03:07 +0300

    Add bounded continuous collision sweep

--- src/parking_rl/physics/__init__.py
+++ src/parking_rl/physics/__init__.py
@@ -1,6 +1,15 @@
 """Deterministic physics primitives for the parking simulator."""
 
 from parking_rl.physics.action_scaling import scale_action
+from parking_rl.physics.ccd import (
+    SweepConfig,
+    SweepResult,
+    bounded_sweep_poses,
+    interpolate_pose,
+    pose_motion_bound_m,
+    sweep_collides,
+    world_step_collides,
+)
 from parking_rl.physics.collision import solid_obstacles, state_collides
 from parking_rl.physics.dynamics import (
     DynamicsConfig,
@@ -30,13 +39,18 @@ __all__ = [
     "DynamicsConfig",
     "Point2",
     "PolicyStepTrace",
+    "SweepConfig",
+    "SweepResult",
     "advance_actuator",
+    "bounded_sweep_poses",
     "clamp_control",
     "integrate_substep",
+    "interpolate_pose",
     "minimum_turn_radius_m",
     "obb_corners",
     "obb_signed_distance",
     "object_obb",
+    "pose_motion_bound_m",
     "sat_face_normal_gap",
     "sat_overlap",
     "sat_penetration_depth",
@@ -44,7 +58,9 @@ __all__ = [
     "solid_obstacles",
     "state_collides",
     "step_world_state",
+    "sweep_collides",
     "vehicle_farthest_corner_m",
     "vehicle_front_extent_m",
     "vehicle_obb",
+    "world_step_collides",
 ]
